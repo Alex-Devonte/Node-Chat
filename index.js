2e852fc95f81d52eb35e08288ef2012f101eb152
Alex Taylor <alexdevontetaylor@gmail.com>
2020-01-02 15:26:00 -0500

    Added server side code to deal with CORS request blocking

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const io = require('socket.io')(http, { origins: '*:*'});
 
 app.get('/', function(req, res) {
   res.render('index.ejs');

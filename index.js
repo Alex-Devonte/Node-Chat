64a8501a6bce678465fea60fd5b3cf7a741cf7d8
Alex Taylor <alexdevontetaylor@gmail.com>
2020-01-02 14:30:36 -0500

    Intital commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.get('/', function(req, res) {
+  res.render('index.ejs');
+});
+
+app.engine('html', require('ejs').renderFile);
+app.use(express.static("public"));
+
+io.sockets.on('connection', function(socket) {
+  socket.on('username', function(username) {
+    socket.username = username;
+
+    io.emit('is_online', '' + socket.username + ' has joined');
+
+    socket.on('typing', () => {
+      socket.broadcast.emit('typing', {username : socket.username});
+    })
+
+    socket.on('stopped typing', () => {
+      socket.broadcast.emit('stopped typing', {username : socket.username});
+    });
+
+    socket.on('chat_message', (data) => {
+      io.emit('chat_message', {message: data.message, username: socket.username});
+    });  
+  });
+
+  socket.on('disconnect', function() {
+    io.emit('is_online', '' + socket.username + ' has left');
+  })
+});
+
+const sever = http.listen(8080, function() {
+  console.log('listening on 8080');
+});
---
+++ public/js/chat.js
@@ -0,0 +1,69 @@
+var socket = io.connect('http://localhost:8080');
+var username = prompt('Type in your username for this session: ');
+var isTypingNotification = $("#is-typing");
+var messageBox = $("#send-message-box");
+var messages = $("#messages");
+
+var typing = false;
+var timeout = "";
+
+socket.emit('username', username);
+//Add each new user to the notifications list
+socket.on('is_online', function(username) {
+  $('#notifications ul').append($('<li>').html(username));
+});
+
+function typingTimeout() {
+  typing = false;
+  socket.emit('stopped typing');
+}
+
+messageBox.on("keypress", () => {
+  if (typing == false) {
+    typing = true;
+    socket.emit("typing");
+    timeout = setTimeout(typingTimeout, 1500);
+  } else {
+    clearTimeout(timeout);
+    timeout = setTimeout(typingTimeout, 1500);
+  }
+});
+
+socket.on("typing", (data) => {
+  isTypingNotification.html(data.username + " is typing . . .").show();
+});
+
+socket.on("stopped typing", (data) => {
+    isTypingNotification.html(data.username + " stopped typing . . .").show();
+});     
+
+socket.on("chat_message", function(data) {
+  data.sender = false;
+
+  if (data.username == username) {
+    data.sender = true;
+  }
+
+  var outgoingTemplate = '<div class="outgoing-message-container message-container"><p class="username">' + data.username + '</p><div class="outgoing-bubble bubble">' + data.message + '</div>';
+  var incomingTemplate = '<div class="incoming-message-container message-container"><p class="username">' + data.username + '</p><div class="incoming-bubble bubble">' + data.message + '</div>';
+  
+  //Append and apply the appropriate styling depending on who sent the message
+  data.sender ? messages.append(outgoingTemplate) : messages.append(incomingTemplate);
+
+  /*Hide the username from subsequent messages if sent from the same user*/
+  if ($('.incoming-message-container').next(".incoming-message-container"))
+  {
+    $(".incoming-message-container").next(".incoming-message-container").find(".username").hide()
+  }
+  if ($('.outgoing-message-container').next(".outgoing-message-container"))
+  {
+    $(".outgoing-message-container").next(".outgoing-message-container").find(".username").hide()
+  }
+});
+
+$('#chat-form').submit(function(e) {
+  e.preventDefault();   
+  socket.emit('chat_message', {message: messageBox.val(), username: username});
+  messageBox.val('');
+  return false;
+});      
